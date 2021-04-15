c030582b01d30c907fc87b05c1f22b600012ad2e
TheExplosivePanda <43807128+TheExplosivePanda@users.noreply.github.com>
2021-04-15 21:06:10 +0300

    Add files via upload

--- ReaperRenamer.cs
+++ ReaperRenamer.cs
@@ -19,22 +19,38 @@ namespace EnemyRenamer
 
         void Start()
         {
-            string stringForInt = "the store is now closed, please leave the building ";
+            string text = "the store is now closed, please leave the building";
 
             GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(BraveResources.Load("DamagePopupLabel", ".prefab"), GameUIRoot.Instance.transform);
 
             Vector3 worldPosition = this.transform.position;
+
             dfLabel = gameObject.GetComponent<dfLabel>();
             dfLabel.gameObject.SetActive(true);
-            dfLabel.Text = stringForInt;
+            dfLabel.Text = text;
             dfLabel.Color = Color.red;
-            dfLabel.Opacity = 1f;
+            dfLabel.Opacity = Larry.opacityAmount;
+            dfLabel.TextScale = Larry.nameSize;
             dfLabel.transform.position = dfFollowObject.ConvertWorldSpaces(worldPosition, GameManager.Instance.MainCameraController.Camera, GameUIRoot.Instance.Manager.RenderCamera).WithZ(0f);
             dfLabel.transform.position = dfLabel.transform.position.QuantizeFloor(dfLabel.PixelsToUnits() / (Pixelator.Instance.ScaleTileScale / Pixelator.Instance.CurrentTileScale));
-
+            NamerModuleClassic.onNameSizeChanged += this.OnNameSizeChanged;
+            NamerModuleClassic.onOpacityAmountChanged += this.OnOpacityAmountChanged;
 
         }
-
+        void OnNameSizeChanged()
+        {
+            if (dfLabel != null)
+            {
+                dfLabel.TextScale = Larry.nameSize;
+            }           
+        }
+        void OnOpacityAmountChanged()
+        {
+            if (dfLabel != null)
+            {
+                dfLabel.Opacity = Larry.opacityAmount;
+            }
+        }
         void Update()
         {
             Vector3 worldPosition = this.transform.position;
