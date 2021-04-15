5d084c014112b51a4da510a8cf036845e506cf32
TheExplosivePanda <43807128+TheExplosivePanda@users.noreply.github.com>
2021-04-15 21:11:19 +0300

    Update Larry.cs

--- Larry.cs
+++ Larry.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,33 +9,63 @@ namespace EnemyRenamer
 {
     class Larry : MonoBehaviour
     {
-        dfLabel dfLabel = null;
-        public static string[] namesDB = null;
+        dfLabel nameLabel = null;
+        public static List<string> namesDB = null;
         tk2dSprite sprite = null;
+        public static float nameSize = 3;
+        public static float opacityAmount = 0.9f;
 
         void Start()
         {
-            string stringForInt = "HI IM LARRY";
+            
+            string text = "HI IM LARRY";
             if (namesDB != null)
             {
-                stringForInt = namesDB[UnityEngine.Random.Range(0, namesDB.Length)];
+                text = namesDB[UnityEngine.Random.Range(0, namesDB.Count)];
             }
             sprite = this.gameObject.GetComponent<tk2dSprite>();
             GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(BraveResources.Load("DamagePopupLabel", ".prefab"), GameUIRoot.Instance.transform);
 
             Vector3 worldPosition = this.transform.position;
-            if (UnityEngine.Random.value > 0.99f)
+
+            nameLabel = gameObject.GetComponent<dfLabel>();
+
+            NamerModuleClassic.onNameSizeChanged += this.OnNameSizeChanged;
+            NamerModuleClassic.onOpacityAmountChanged += this.OnOpacityAmountChanged;
+
+            if (UnityEngine.Random.value > 0.995f)
             {
-                dfLabel.Glitchy = true;
+                nameLabel.Glitchy = true;
             }
-            dfLabel = gameObject.GetComponent<dfLabel>();
-            dfLabel.gameObject.SetActive(true);
-            dfLabel.Text = stringForInt;
-            dfLabel.Color = Color.HSVToRGB(UnityEngine.Random.value, 1.0f, 1.0f);
-            dfLabel.Opacity = 1f;
-            dfLabel.transform.position = dfFollowObject.ConvertWorldSpaces(worldPosition, GameManager.Instance.MainCameraController.Camera, GameUIRoot.Instance.Manager.RenderCamera).WithZ(0f);
-            dfLabel.transform.position = dfLabel.transform.position.QuantizeFloor(dfLabel.PixelsToUnits() / (Pixelator.Instance.ScaleTileScale / Pixelator.Instance.CurrentTileScale));
+            nameLabel.gameObject.SetActive(true);
+            nameLabel.Text = text;
+            nameLabel.Color = Color.HSVToRGB(UnityEngine.Random.value, 1.0f, 1.0f);
+            nameLabel.Opacity = Larry.opacityAmount;
+            nameLabel.transform.position = dfFollowObject.ConvertWorldSpaces(worldPosition, GameManager.Instance.MainCameraController.Camera, GameUIRoot.Instance.Manager.RenderCamera).WithZ(0f);
+            nameLabel.transform.position = nameLabel.transform.position.QuantizeFloor(nameLabel.PixelsToUnits() / (Pixelator.Instance.ScaleTileScale / Pixelator.Instance.CurrentTileScale));
+            nameLabel.TextScale = Larry.nameSize;
+            xOffSet = CalculateCenterXoffset(nameLabel);
         }
+        void OnOpacityAmountChanged()
+        {
+            if (nameLabel != null)
+            {
+                nameLabel.Opacity = Larry.opacityAmount;
+            }
+        }
+        void OnNameSizeChanged()
+        {
+            if (this.nameLabel != null )
+            {
+                this.nameLabel.TextScale = nameSize;
+                xOffSet = CalculateCenterXoffset(nameLabel);               
+            }           
+        }
+        float CalculateCenterXoffset(dfLabel label)
+        {
+            return label.GetCenter().x - label.transform.position.x;
+        }
+        float xOffSet = 0;
 
         void Update()
         {
@@ -44,13 +74,17 @@ namespace EnemyRenamer
             {
                 worldPosition = sprite.WorldBottomCenter;
             }
-
-            dfLabel.transform.position = dfFollowObject.ConvertWorldSpaces(worldPosition, GameManager.Instance.MainCameraController.Camera, GameUIRoot.Instance.Manager.RenderCamera).WithZ(0f);
-            dfLabel.transform.position = dfLabel.transform.position.QuantizeFloor(dfLabel.PixelsToUnits() / (Pixelator.Instance.ScaleTileScale / Pixelator.Instance.CurrentTileScale));
+            if (nameLabel == null)
+            {
+                ETGModConsole.Log("yeah its back magic aight?");
+            }
+            Vector2 tempPos = dfFollowObject.ConvertWorldSpaces(worldPosition, GameManager.Instance.MainCameraController.Camera, GameUIRoot.Instance.Manager.RenderCamera).WithZ(0f);
+            nameLabel.transform.position = tempPos.WithX(tempPos.x - xOffSet);
+            nameLabel.transform.position = nameLabel.transform.position.QuantizeFloor(nameLabel.PixelsToUnits() / (Pixelator.Instance.ScaleTileScale / Pixelator.Instance.CurrentTileScale));
         }
         void OnDestroy()
         {
-            UnityEngine.Object.Destroy(dfLabel.gameObject);
+            UnityEngine.Object.Destroy(nameLabel.gameObject);
         }
     }
 }
